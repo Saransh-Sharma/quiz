184ef5a5b9e9fca2208b49cc1bd12f2064525286
Saransh <saransh@BMDVSARANSHSH-2.local>
2024-09-30 16:30:12 +0530

    add start quiz button

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/start_screen.dart';
 
 void main() {
-  runApp(const QuizApp());
-}
-
-class QuizApp extends StatelessWidget {
-  const QuizApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Quiz App',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.white),
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 101, 28, 228),
+          Color.fromARGB(255, 107, 99, 131)
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight)),
+        child: const StartScreen(),
       ),
-      home: const StartScreen(),
-    );
-  }
-}
-
-class StartScreen extends StatelessWidget {
-  const StartScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildLogo(),
-            const SizedBox(height: 40),
-            _buildWelcomeText(),
-            const SizedBox(height: 20),
-            _buildStartButton(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLogo() {
-    return Image.asset(
-      'assets/images/quiz-logo.png',
-      width: 300,
-      height: 400,
-    );
-  }
-
-  Widget _buildWelcomeText() {
-    return const Text(
-      'Learn Flutter the fun way!',
-      style: TextStyle(
-        fontSize: 28,
-      ),
-    );
-  }
-
-  Widget _buildStartButton() {
-    return ElevatedButton(
-      onPressed: () {
-        // TODO: Implement quiz start functionality
-      },
-      child: const Text('Start Quiz'),
-    );
-  }
+    ),
+  ));
 }
