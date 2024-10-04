51834e1c09769bfa7c03afa55f6a6fea389029cb
Saransh <saransh@BMDVSARANSHSH-2.local>
2024-10-04 14:43:31 +0530

    add quiz and questions stateful widget stubs

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/start_screen.dart';
+import 'package:quiz/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 101, 28, 228),
-          Color.fromARGB(255, 107, 99, 131)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight)),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(Quiz());
 }
