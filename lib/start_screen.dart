1a6fd98030f5fc28621b17f48a1cd0cd45dcf006
Saransh <saransh@BMDVSARANSHSH-2.local>
2024-10-04 13:11:43 +0530

    add transparency to quiz image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,17 +7,21 @@ class StartScreen extends StatelessWidget {
   Widget build(context) {
     return Center(
       child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Image.asset('assets/images/quiz-logo.png', width: 300),
+        Image.asset('assets/images/quiz-logo.png', width: 300, color: const Color.fromARGB(150, 250, 250, 250),),
         const SizedBox(height: 80),
         const Text(
           'Learn Flutter the fun way !',
           style: TextStyle(color: Colors.white, fontSize: 28),
         ),
         const SizedBox(height: 30),
-        OutlinedButton(
+        OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz', style: TextStyle(fontSize: 24),)),
+            // icon: Icon()
+            label: const Text(
+              'Start Quiz',
+              style: TextStyle(fontSize: 24),
+            )),
       ]),
     );
   }
